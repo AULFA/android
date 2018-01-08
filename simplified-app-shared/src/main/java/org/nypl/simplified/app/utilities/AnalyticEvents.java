03d5d07ef9777e152797cbe0425cbaff24015c76
SKULLBONEZ-PC\Skullbonez <s.eschbach@gmail.com>
2018-01-09 08:18:11 +1000

    Fixing event naming error for Firebase.

--- simplified-app-shared/src/main/java/org/nypl/simplified/app/utilities/AnalyticEvents.java
+++ simplified-app-shared/src/main/java/org/nypl/simplified/app/utilities/AnalyticEvents.java
@@ -10,7 +10,7 @@ public final class AnalyticEvents {
   public static class Event {
     public static String BOOK_OPENED = "BOOK_OPENED";
     public static String BOOK_DOWNLOADED = "BOOK_DOWNLOADED";
-    public static String CATALOG_SEARCHED = "SEARCH_QUERY";
+    public static String CATALOG_SEARCHED = "CATALOG_SEARCHED";
   }
 
   public static class StringParameter {
